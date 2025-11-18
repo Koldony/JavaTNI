a7b8ce9fcb26bf4df4df8471f3274f5baf68369b
Koldony <inkatiwit@gmail.com>
2025-11-18 13:45:18 +0700

    submit excercise LabSheet101

---
+++ LabSheet01/src/Lab101.java
@@ -0,0 +1,7 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Atiwit Seedee");
+        //System.out.println("\"INT-108\" 'Object-Oriented Programming'");
+        System.out.println("\tThis is my first time learning Java\n\tI hope to get grade \"A\"");
+    }
+}
